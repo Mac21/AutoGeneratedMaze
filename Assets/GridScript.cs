a7c27cbb08559c2fd68e556a9bb29b654736c1fa
Mac21 <russ.tolle@gmail.com>
2014-02-02 22:27:09 -0700

    Initial Commit

---
+++ Assets/GridScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridScript : MonoBehaviour
+{
+
+  public Transform CellPrefab;
+  public Vector3 Size;
+
+  // Use this for initialization
+  void Start()
+  {
+    CreateGrid();
+  }
+
+  void CreateGrid()
+  {
+    for (int x = 0; x < Size.x; x++)
+    {
+      for (int z = 0; z < Size.z; z++)
+      {
+        Instantiate(CellPrefab, new Vector3(x, 0, z), Quaternion.identity);
+      }
+    }
+  }
+}
